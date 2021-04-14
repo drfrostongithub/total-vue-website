c3f68588a974e007ad6d4ca66ae69bc9682f5f99
Putra Reza Fardani <devtcp.pfardani@gmail.com>
2021-04-14 14:18:25 +0700

    Pre-Design

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,124 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <body>
+    <Navbar />
     <router-view />
-  </div>
+    <Footer />
+  </body>
 </template>
 
+<script>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    Footer,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+$colourText: #353f44;
+$colourHeading: #384548;
+$colourNotification: #42bfdc;
+$colourPositive: #34aa44;
+$colourNegative: #f85359;
+
+@mixin border-radius($radius) {
+  -webkit-border-radius: $radius;
+  -moz-border-radius: $radius;
+  border-radius: $radius;
+}
+html,
+body,
+#KmanSoftwareCoreContainer {
+  height: 100%;
+  scroll-behavior: smooth;
 }
 
-#nav {
-  padding: 30px;
+body {
+  background-color: #f5f5f5;
+  margin: 0;
+  padding: 0;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+body,
+p,
+li,
+input,
+select {
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  color: $colourText;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+a,
+a:visited {
+  text-decoration: none;
+}
+
+a:focus {
+  outline: none;
+}
+
+b,
+strong {
+  font-weight: 700;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: canada-type-gibson, sans-serif;
+  font-style: normal;
+  color: $colourHeading;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-weight: 400;
+}
+
+h5,
+h6 {
+  font-weight: 600;
+}
+
+input {
+  &:focus {
+    outline: none;
   }
+}
+
+small {
+  font-size: 0.75rem;
+}
+
+.text-left {
+  text-align: left;
+}
+
+.text-center {
+  text-align: center;
+}
+
+.text-right {
+  text-align: right;
+}
+
+.no-margin {
+  margin: 0 !important;
+}
+
+.no-radius {
+  border-radius: 0 !important;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <div>Nav</div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+<style>
+nav {
+  border: 1px solid blue;
+}
+</style>
